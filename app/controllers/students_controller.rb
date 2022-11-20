5f8db7984393984e44db1c8e67b43b5f6603e903
Felix <barosiofelix@gmail.com>
2022-11-20 15:04:30 +0300

    Completed Assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+
+    students = if params[:name]
+                  Student.find_by(first_name: params[:name].downcase.titleize) || Student.find_by(last_name: params[:name].downcase.titleize)
+                else
+                  Student.all
+                end
     render json: students
+    # byebug
   end
 
   def show
